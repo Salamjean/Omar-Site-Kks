b7fc667ce9e870abcf1d08d72d83edef0f0a05c6
SalamJean <salamjeanlouis8@gmail.com>
2025-04-13 14:32:13 +0000

    fix: error1

--- resources/views/commandes/indexAdmin.blade.php
+++ resources/views/commandes/indexAdmin.blade.php
@@ -41,7 +41,7 @@
 @endif
 <div class="content-wrapper">
     <div class="container-xxl flex-grow-1 container-p-y">
-      <h4 class="fw-bold py-3 mb-4 text-center">Listes de toutes les commandes</h4>
+      <h4 class="fw-bold py-3 mb-4 text-center">Listes de toutes les commandes payées</h4>
 
       <div class="card">
         <div class="d-flex justify-content-between">
